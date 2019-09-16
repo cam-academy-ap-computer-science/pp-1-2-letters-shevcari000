b81590f2d9c2888b833cfbe270d5362fafd0a3c7
shevcari000 <shevcari000@CAM-LAB-27.bgsd.k12.wa.us>
2019-09-16 07:53:24 -0700

    Started

--- src/Letters.java
+++ src/Letters.java
@@ -3,7 +3,12 @@ public class Letters {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        ToGrandpa();
+        ToMother();
+        ToFather();
+        
 	}
 
-}
+    public static void ToGrandpa () {
+    	System.out.println (Today is September "15")
+    }
