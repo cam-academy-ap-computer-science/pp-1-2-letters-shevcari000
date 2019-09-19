6e3d024d9f811141b4a075209aa7ffc18cb3370c
shevcari000 <shevcari000@CAM-LAB-27.bgsd.k12.wa.us>
2019-09-19 07:03:03 -0700

    Finished

--- src/Letters.java
+++ src/Letters.java
@@ -4,11 +4,68 @@ public class Letters {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         ToGrandpa();
+        Space();
+        Space();
         ToMother();
+        Space();
+        Space();
         ToFather();
+        
         
 	}
 
     public static void ToGrandpa () {
-    	System.out.println (Today is September "15")
+    	System.out.println ("To my dear grandpa,");
+    	System.out.println ("");
+    	System.out.println ("	What’s new today? Nothing much, same routine day after day");
+    	System.out.println ("	Wake up, go to school, study, hang with friends, and go back to sleep.");
+    	System.out.println ("	Is there more to life u think? I dont know.");
+    	System.out.println ("	But anyways, how are you?\n");
+    	System.out.println ("	I was thinking about moving, somewhere beachy.");
+    	System.out.println ("	Not a popular place but more just for the peace and quiet.");
+    	System.out.println ("	Nature is a good place to think about life or anything really.");
+    	System.out.println ("	I'm starting to listen to music often now.");
+    	System.out.println ("	You can relate to many songs and artists express many feelings through them.");
+    	System.out.println ("	Hope to hear from you soon!\n");
+    	System.out.println ("Love, Arina");
+    	
     }
+    
+    public static void Space () {
+    	System.out.println ();
+  
+    }
+    
+    public static void ToMother () {
+    	System.out.println ("To my lovely mother,");
+    	System.out.println ("");
+    	System.out.println ("	What’s new today? Nothing much, same routine day after day");
+    	System.out.println ("	Wake up, go to school, study, hang with friends, and go back to sleep.");
+    	System.out.println ("	Is there more to life u think? I dont know.");
+    	System.out.println ("	But anyways, how are you?\n");
+    	System.out.println ("	I was thinking about moving, somewhere beachy.");
+    	System.out.println ("	Not a popular place but more just for the peace and quiet.");
+    	System.out.println ("	Nature is a good place to think about life or anything really.");
+    	System.out.println ("	I'm starting to listen to music often now.");
+    	System.out.println ("	You can relate to many songs and artists express many feelings through them.");
+    	System.out.println ("	Hope to hear from you soon!\n");
+    	System.out.println ("Love, Arina");
+    	
+    }
+    
+    public static void ToFather () {
+    	System.out.println ("To my father,");
+    	System.out.println ("");
+    	System.out.println ("	What’s new today? Nothing much, same routine day after day");
+    	System.out.println ("	Wake up, go to school, study, hang with friends, and go back to sleep.");
+    	System.out.println ("	Is there more to life u think? I dont know.");
+    	System.out.println ("	But anyways, how are you?\n");
+    	System.out.println ("	I was thinking about moving, somewhere beachy.");
+    	System.out.println ("	Not a popular place but more just for the peace and quiet.");
+    	System.out.println ("	Nature is a good place to think about life or anything really.");
+    	System.out.println ("	I'm starting to listen to music often now.");
+    	System.out.println ("	You can relate to many songs and artists express many feelings through them.");
+    	System.out.println ("	Hope to hear from you soon!\n");
+    	System.out.println ("Love, Arina");
+    }
+}
